a434a313cc3e6aed4b050403589356afdccde6ff
Baptiste Dumoulin <Baptiste.Dumoulin@insa-rennes.fr>
2024-01-02 15:31:46 +0100

    create point and path in models and add them to the admin view

--- traffic_app/admin.py
+++ traffic_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Point, Path
+
+admin.site.register(Point)
+admin.site.register(Path)
--- traffic_app/models.py
+++ traffic_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Point(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Path(models.Model):
+    name = models.CharField(max_length=100)
+    start_point = models.ForeignKey(Point, on_delete=models.CASCADE, related_name='start_point')
+    end_point = models.ForeignKey(Point, on_delete=models.CASCADE, related_name='end_point')
+
+    def __str__(self):
+        return f"{self.name} : {self.start_point} to {self.end_point}"
+
