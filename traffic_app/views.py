dd9b2e477e6a80b0560eb7d73d1f7527d96b34c8
Baptiste Dumoulin <Baptiste.Dumoulin@insa-rennes.fr>
2024-01-03 16:44:52 +0100

    -add a select for weather and time in the way page

--- traffic_app/views.py
+++ traffic_app/views.py
@@ -23,13 +23,16 @@ def calculate_best_way(request):
     if request.method == 'POST':
         start_point_id = request.POST.get('start_point')
         end_point_id = request.POST.get('end_point')
+        weather = request.POST.get('weather', 'sun')
+        time_period = request.POST.get('time_period', '10-16')
 
         start_point = Point.objects.get(id=start_point_id)
         end_point = Point.objects.get(id=end_point_id)
 
         # Perform the calculation for the best way
-        best_way = map_instance.get_fastest_path(start_point, end_point, 'sun', '19-24')
+        best_way = map_instance.get_fastest_path(start_point, end_point, weather, time_period)
 
-        return render(request, 'way_result.html', {'start_point': start_point, 'end_point': end_point, 'best_way': best_way})
+        return render(request, 'way_result.html',
+                      {'start_point': start_point, 'end_point': end_point, 'best_way': best_way})
 
     return render(request, 'way.html', {'vertices': vertices})
